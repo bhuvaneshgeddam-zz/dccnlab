ba90cfdccf5f60fa61a40962da0e19cc84c871df
bhuvaneshgeddam <47291535+bhuvaneshgeddam@users.noreply.github.com>
2019-11-29 00:34:40 +0530

    Update bytestuffing.c

--- bytestuffing.c
+++ bytestuffing.c
@@ -1,64 +1,33 @@
 #include<stdio.h>
 #include<string.h>
-int main()
+main()
 {
-	char d[20];
-	char d1[40];
-	int i,j=0;
-	printf("enter data:");
-	scanf("%s",d);
-	d1[j]='f';
-	j++;
-	d1[j]='l';
-	j++;
-	d1[j]='a';
-	j++;
-	d1[j]='g';
-	j++;
-	for(i=0;i<strlen(d);i++)
-	{
-		if(d[i]=='e' && d[i+1]=='s' && d[i+2]=='c')
-		{
-			d1[j]='e';
-			d1[j+1]='s';
-			d1[j+2]='c';
-			d1[j+3]='e';
-			d1[j+4]='s';
-			d1[j+5]='c';
-			j=j+6;
-			i=i+2;
-		}
-		else if(d[i]=='f' && d[i+1]=='l' && d[i+2]=='a' && d[i+3]=='g')
-		{
-			d1[j]='e';
-			d1[j+1]='s';
-			d1[j+2]='c';
-			d1[j+3]='f';
-			d1[j+4]='l';
-			d1[j+5]='a';
-			d1[j+6]='g';
-			j=j+7;
-			i=i+3;
-		}
-		else
-		{
-			d1[j]=d[i];
-			j++;
-		}	
-	}
-	d1[j]='f';
-	j++;
-	d1[j]='l';
-	j++;
-	d1[j]='a';
-	j++;
-	d1[j]='g';
-	j++;
-	printf("after byte stuffing:");
-	for(i=0;i<j;i++)
-		printf(" %c",d1[i]);
-	printf("\n");
-}
-/*enter data:aafflagesc
-after byte stuffing: a a f f l a g f l a g e s c e s c
-*/
+        char a[30], fs[50] = " ", t[3], sd, ed, x[3], s[3], d[3], y[3];
+        int i, j, p = 0, q = 0;
+        clrscr();
+        printf("Enter characters to be stuffed:");
+        scanf("%s", a);
+        printf("\nEnter a character that represents starting delimiter:");
+        scanf(" %c", &sd);
+        printf("\nEnter a character that represents ending delimiter:");
+        scanf(" %c", &ed);
+        x[0] = s[0] = s[1] = sd;
+        x[1] = s[2] = '\0';
+        y[0] = d[0] = d[1] = ed;
+        d[2] = y[1] = '\0';
+        strcat(fs, x);
+        for(i = 0; i < strlen(a); i++)
+        {
+            t[0] = a[i];
+            t[1] = '\0';
+            if(t[0] == sd)
+                strcat(fs, s);
+            else if(t[0] == ed)
+                strcat(fs, d);
+            else
+                strcat(fs, t);
+        }
+        strcat(fs, y);
+        printf("\n After stuffing:%s", fs);
+        getch();
+    }
